03facd18ecb9cd6541e19e1a78e60bcc02cbe94b
Eron Salling <esalling23@gmail.com>
2024-01-08 13:27:54 -0500

    Updates boxtracker & tracker controllers to use promises

--- api/controllers/boxTrackerController.js
+++ api/controllers/boxTrackerController.js
@@ -4,61 +4,91 @@ const { model } = require('mongoose');
 
 const BoxTracker = model('BoxTracker')
 
-// Get ALL boxHabits
-const index = function(req, res) {
-  BoxTracker.find({}, (err, boxTracker) => {
-    if (err) res.send(err)
-
-    res.send(boxTracker)
-  })
+// INDEX
+// GET /boxTrackers
+const index = (req, res, next) => {
+  BoxTracker.find()
+    .then(boxTrackers => {
+      // `boxTrackers` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return boxTrackers.map(boxTracker => boxTracker.toObject())
+    })
+    // respond with status 200 and JSON of the boxTrackers
+    .then(boxTrackers => res.status(200).json({ boxTrackers }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
 }
 
-// Get ONE boxTracker
-const show = function(req, res) {
-  BoxTracker.findById(req.params.id, (err, boxTracker) => {
-    if (err) res.send(err)
-
-    res.send(boxTracker)
-  })
+// SHOW
+// GET /boxTrackers/5a7db6c74d55bc51bdf39793
+const show = (req, res, next) => {
+  // req.params.id will be set based on the `:id` in the route
+  BoxTracker.findById(req.params.id)
+    .then(handle404)
+    // if `findById` is succesful, respond with 200 and "boxTracker" JSON
+    .then(boxTracker => res.status(200).json({ boxTracker: boxTracker.toObject() }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
 }
 
-// Create boxTracker
-const create = function(req, res) {
-  console.log('creating new boxTracker')
-  const newhabit = new BoxTracker(req.body)
-  newhabit.save((err, boxTracker) => {
-    console.log(err, boxTracker)
-    if (err) res.send(err)
+// CREATE
+// POST /boxTrackers
+const create = (req, res, next) => {
+  // set owner of new boxTracker to be current user
+  req.body.boxTracker.owner = req.user.id
 
-    res.send(boxTracker)
-  })
+  BoxTracker.create(req.body.boxTracker)
+    // respond to succesful `create` with status 201 and JSON of new "boxTracker"
+    .then(boxTracker => {
+      res.status(201).json({ boxTracker: boxTracker.toObject() })
+    })
+    // if an error occurs, pass it off to our error handler
+    // the error handler needs the error message and the `res` object so that it
+    // can send an error message back to the client
+    .catch(next)
 }
 
-// Update boxTracker
-const update = function(req, res) {
-  BoxTracker.findOneAndUpdate({
-    _id: req.params.id
-  },
-  req.body,
-  { new: true },
-  (err, boxTracker) => {
-    if (err) res.send(err)
+// UPDATE
+// PATCH /boxTrackers/5a7db6c74d55bc51bdf39793
+const update = (req, res, next) => {
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.boxTracker.owner
 
-    res.send(boxTracker)
-  })
+  BoxTracker.findById(req.params.id)
+    .then(handle404)
+    .then(boxTracker => {
+      // pass the `req` object and the Mongoose record to `requireOwnership`
+      // it will throw an error if the current user isn't the owner
+      requireOwnership(req, boxTracker)
+
+      // pass the result of Mongoose's `.update` to the next `.then`
+      return boxTracker.update(req.body.boxTracker)
+    })
+    // if that succeeded, return 204 and no JSON
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
 }
 
-// Delete boxTracker
-const destroy = function(req, res) {
-  BoxTracker.remove({
-    _id: req.params.id
-  },
-  (err, boxTracker) => {
-    if (err) res.send(err)
-
-    res.send(boxTracker)
-  })
+// DESTROY
+// DELETE /boxTrackers/5a7db6c74d55bc51bdf39793
+const destroy = (req, res, next) => {
+  BoxTracker.findById(req.params.id)
+    .then(handle404)
+    .then(boxTracker => {
+      // throw an error if current user doesn't own `boxTracker`
+      requireOwnership(req, boxTracker)
+      // delete the boxTracker ONLY IF the above didn't throw
+      boxTracker.remove()
+    })
+    // send back 204 and no content if the deletion succeeded
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
 }
+
 
 module.exports = {
   index,
--- api/controllers/trackerController.js
+++ api/controllers/trackerController.js
@@ -4,61 +4,91 @@ const { model } = require('mongoose');
 
 const Tracker = model('Tracker')
 
-// Get ALL habits
-const index = function(req, res) {
-  Tracker.find({}, (err, habits) => {
-    if (err) res.send(err)
-
-    res.send(habits)
-  })
+// INDEX
+// GET /trackers
+const index = (req, res, next) => {
+  Tracker.find()
+    .then(trackers => {
+      // `trackers` will be an array of Mongoose documents
+      // we want to convert each one to a POJO, so we use `.map` to
+      // apply `.toObject` to each one
+      return trackers.map(tracker => tracker.toObject())
+    })
+    // respond with status 200 and JSON of the trackers
+    .then(trackers => res.status(200).json({ trackers }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
 }
 
-// Get ONE tracker
-const show = function(req, res) {
-  Tracker.findById(req.params.id, (err, tracker) => {
-    if (err) res.send(err)
-
-    res.send(tracker)
-  })
+// SHOW
+// GET /trackers/5a7db6c74d55bc51bdf39793
+const show = (req, res, next) => {
+  // req.params.id will be set based on the `:id` in the route
+  Tracker.findById(req.params.id)
+    .then(handle404)
+    // if `findById` is succesful, respond with 200 and "tracker" JSON
+    .then(tracker => res.status(200).json({ tracker: tracker.toObject() }))
+    // if an error occurs, pass it to the handler
+    .catch(next)
 }
 
-// Create tracker
-const create = function(req, res) {
-  console.log('creating new tracker')
-  const newhabit = new Tracker(req.body)
-  newhabit.save((err, tracker) => {
-    console.log(err, tracker)
-    if (err) res.send(err)
+// CREATE
+// POST /trackers
+const create = (req, res, next) => {
+  // set owner of new tracker to be current user
+  req.body.tracker.owner = req.user.id
 
-    res.send(tracker)
-  })
+  Tracker.create(req.body.tracker)
+    // respond to succesful `create` with status 201 and JSON of new "tracker"
+    .then(tracker => {
+      res.status(201).json({ tracker: tracker.toObject() })
+    })
+    // if an error occurs, pass it off to our error handler
+    // the error handler needs the error message and the `res` object so that it
+    // can send an error message back to the client
+    .catch(next)
 }
 
-// Update tracker
-const update = function(req, res) {
-  Tracker.findOneAndUpdate({
-    _id: req.params.id
-  },
-  req.body,
-  { new: true },
-  (err, tracker) => {
-    if (err) res.send(err)
+// UPDATE
+// PATCH /trackers/5a7db6c74d55bc51bdf39793
+const update = (req, res, next) => {
+  // if the client attempts to change the `owner` property by including a new
+  // owner, prevent that by deleting that key/value pair
+  delete req.body.tracker.owner
 
-    res.send(tracker)
-  })
+  Tracker.findById(req.params.id)
+    .then(handle404)
+    .then(tracker => {
+      // pass the `req` object and the Mongoose record to `requireOwnership`
+      // it will throw an error if the current user isn't the owner
+      requireOwnership(req, tracker)
+
+      // pass the result of Mongoose's `.update` to the next `.then`
+      return tracker.update(req.body.tracker)
+    })
+    // if that succeeded, return 204 and no JSON
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
 }
 
-// Delete tracker
-const destroy = function(req, res) {
-  Tracker.remove({
-    _id: req.params.id
-  },
-  (err, tracker) => {
-    if (err) res.send(err)
-
-    res.send(tracker)
-  })
+// DESTROY
+// DELETE /trackers/5a7db6c74d55bc51bdf39793
+const destroy = (req, res, next) => {
+  Tracker.findById(req.params.id)
+    .then(handle404)
+    .then(tracker => {
+      // throw an error if current user doesn't own `tracker`
+      requireOwnership(req, tracker)
+      // delete the tracker ONLY IF the above didn't throw
+      tracker.remove()
+    })
+    // send back 204 and no content if the deletion succeeded
+    .then(() => res.sendStatus(204))
+    // if an error occurs, pass it to the handler
+    .catch(next)
 }
+
 
 module.exports = {
   index,
